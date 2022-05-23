97c50d3ff0c9cdb561861c84c2bf84c1544d0729
Samuel Leal <samuelmarquessousaleal@gmail.com>
2022-05-23 14:09:20 -0300

    Config

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6038abaf9b01122dedfacc4a3728aa63'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '694b6ea2-a843-4f2f-88b9-3fcee9b44c92'
+};
